a7b01cf1ca6f637998d6dd6f534600dfdda0a540
n-elmasry <almassrynada411@gmail.com>
2023-10-31 12:47:47 -0700

    tasks

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+
+    print(result)
