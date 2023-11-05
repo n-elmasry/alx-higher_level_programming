72df361bddd6b4d44f2f6ec6d2965dbdc30097c3
n-elmasry <almassrynada411@gmail.com>
2023-11-05 09:55:20 -0800

    tasks

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len1 = len(sentence)
-    if len1 == 0:
-        firstChar = None
+    if len(sentence) == 0:
+        tup = (0, None)
     else:
-        firstChar = sentence[0]
-
-    return(len1, firstChar)
+        tup = (len(sentence), sentence[0])
+    return(tup)
