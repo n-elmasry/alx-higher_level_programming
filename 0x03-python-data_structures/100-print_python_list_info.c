e00aaffb0406ebf72546c8439bc7b0f7efbde3c0
n-elmasry <almassrynada411@gmail.com>
2023-11-07 04:08:56 -0800

    advanded

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,25 @@
+#include <Python.h>
 /**
- * print_python_list_info - prints some basic info about Python lists.
- * @p: pointer
- * Return: void
- */
-void print_python_list_info(PyObject *p)
-{
+* print_python_list_info - prints some basic info about Python lists.
+* @p: pointer
+* Return: void
+*/
+void print_python_list_info(PyObject *p) {
+    
+    if (PyList_Check(p)) {
+        PyListObject *list = (PyListObject *)p;
+
+    
+        printf("[*] Size of the Python List = %ld\n", Py_SIZE(list));
+        printf("[*] Allocated = %ld\n", list->allocated);
+
+        
+        for (int i = 0; i < Py_SIZE(list); i++) {
+            PyObject *item = PyList_GetItem(p, i);
+            printf("Element %d: %s\n", i, item->ob_type->tp_name);
+        }
+    } else {
+        
+        fprintf(stderr, "Invalid List Object\n");
+    }
 }
