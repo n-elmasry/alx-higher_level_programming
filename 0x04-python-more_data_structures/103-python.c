6cedfde6ef53eaa00a8aa74218c7db46b158bdff
n-elmasry <almassrynada411@gmail.com>
2023-11-08 13:21:31 -0800

    tasks

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,8 @@
+/**
+ * print_python_list - checks if a singly linked list is a palindrome.
+ * @p: pointer to the head of the list
+ * Return: 1 if it is a palindrome, 0 otherwise
+ */
+void print_python_list(PyObject *p)
+{
+}
