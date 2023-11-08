e9d3ce82919b62d7873148caa360ca7521667a26
n-elmasry <almassrynada411@gmail.com>
2023-11-08 12:54:39 -0800

    tasks

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+def multiply_list_map(my_list=[], number=0):
+    def x(num):
+        return num * number
+    new_list = []
+    new_list = list(map(x, my_list))
+    return new_list
