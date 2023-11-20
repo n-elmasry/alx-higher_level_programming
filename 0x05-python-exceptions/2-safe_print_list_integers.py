b1092d81e4ed0b56274c93b19985d9fc82b4067f
n-elmasry <almassrynada411@gmail.com>
2023-11-20 06:18:12 -0800

    tasks

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end='')
+                count += 1
+        print()
+        return count
+    except TypeError:
+        print("TypeError: All elements must be integers")
+    except IndexError:
+        raise
