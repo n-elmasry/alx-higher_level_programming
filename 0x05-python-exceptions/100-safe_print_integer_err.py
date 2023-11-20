1121c25da3bfab700de843e0a659188e2e19e3a6
n-elmasry <almassrynada411@gmail.com>
2023-11-20 13:29:28 -0800

    task7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        message = "Exception: Unknown format code 'd' for object of type 'str'"
-        print(f"message", file=sys.stderr)
+        message = f"Exception: {e}"
+        print(message, file=sys.stderr)
         return False
