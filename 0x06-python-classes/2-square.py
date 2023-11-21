f5bacf58544b59e7b040669d7038ac9bb6603b90
n-elmasry <almassrynada411@gmail.com>
2023-11-21 09:47:46 -0800

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""creates class Square"""
+
+
+class Square:
+    """defines a square """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
