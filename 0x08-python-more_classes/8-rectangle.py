30473bbf5915f30fc5a62416ff7b1797f527008b
n-elmasry <almassrynada411@gmail.com>
2023-11-27 10:36:05 -0800

    task8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
 
         if not isinstance(rect_1, Rectangle):
             raise TypeError('rect_1 must be an instance of Rectangle')
-        elif not isinstance(rect_2, Rectangle):
+        if not isinstance(rect_2, Rectangle):
             raise TabError('rect_2 must be an instance of Rectangle')
 
         result1 = rect_1.area()
