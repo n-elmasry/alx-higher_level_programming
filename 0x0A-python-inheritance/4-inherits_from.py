e8cfd2ca81bc74fadef6c9570445c2fed0fbba2d
n-elmasry <almassrynada411@gmail.com>
2023-12-04 02:23:28 -0800

    tasks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""True if object is an instance of a class that inherited from the class"""
 
+
+def inherits_from(obj, a_class):
+    """ returns True or False"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+""" a class BaseGeometry """
 
+
+class BaseGeometry:
+    """ a class BaseGeometry """
+    def __init__(self) -> None:
+        pass
+
+    def area(self):
+        raise Exception('area() is not implemented')
