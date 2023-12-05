36fbff376a29aa6acbaa033bbb528fccb58a3e3d
n-elmasry <almassrynada411@gmail.com>
2023-12-05 12:25:56 -0800

    task8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""   returns the dictionary description  """
 
+
+def class_to_json(obj):
+    """  returns the dictionary description  """
+    newdict = {}
+
+    for attr in obj.__dict__:
+        attrvalue = getattr(obj, attr)
+        if isinstance(attrvalue, (list, dict, str, int, bool)):
+            newdict[attr] = attrvalue
+    return newdict
