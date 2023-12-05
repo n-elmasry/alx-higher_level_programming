ec17129e4f6e4cd475bac0bfac7c3c486ead1087
n-elmasry <almassrynada411@gmail.com>
2023-12-05 09:24:44 -0800

    task5

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""  creates an Object from a JSON file """
+import json
 
+
+def load_from_json_file(filename):
+    """ creates an Object from a JSON file """
+    with open(filename, 'r') as fle:
+        new_json = json.load(fle)
+        return new_json
