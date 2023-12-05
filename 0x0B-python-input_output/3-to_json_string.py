3dc0a8fadfd40e799f8de7edef12ff3ac4ca741c
n-elmasry <almassrynada411@gmail.com>
2023-12-05 08:42:07 -0800

    task3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""  returns the JSON representation of an object (string) """
+import json
 
+
+def to_json_string(my_obj):
+    """ returns the JSON representation """
+    new_json = json.dumps(my_obj)
+    return new_json
