b9d955966d31e78bcfda738619fa7bead8626712
n-elmasry <almassrynada411@gmail.com>
2023-12-05 12:48:08 -0800

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""  class Student that defines a student """
 
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        newdict = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+
+        return newdict
