b4ba9623052f8e0fbd3df4f005e62880f68398a6
n-elmasry <almassrynada411@gmail.com>
2023-12-05 04:36:08 -0800

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""  function that reads a text file (UTF8) and prints it to stdout """
+import sys
+
+
+def read_file(filename=""):
+    """ reads a text file """
+    with open(filename, "r",  encoding="utf-8") as fle:
+        for lines in fle.readlines():
+            print(lines, end='', file=sys.stdout)
