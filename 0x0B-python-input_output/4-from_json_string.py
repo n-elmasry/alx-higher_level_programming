4cf30dbc6b9d20736ea3be3c2ca0cc10e9a37b90
n-elmasry <almassrynada411@gmail.com>
2023-12-05 08:45:15 -0800

    task4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""  returns an object represented by a JSON string """
+import json
 
+
+def from_json_string(my_str):
+    """ returns an object """
+    new_json = json.loads(my_str)
+    return new_json
