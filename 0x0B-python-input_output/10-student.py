ecaf46de9c02ac33408275410361457bbe40cfd2
n-elmasry <almassrynada411@gmail.com>
2023-12-05 13:09:16 -0800

    task10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
+"""  class Student that defines a student """
 
+
+class Student:
+    """ class Student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        newdict = {}
+
+        if isinstance(attrs, list):
+            for attr in attrs:
+                if hasattr(self, attr):
+                    newdict[attr] = getattr(self, attr)
+
+        else:
+            newdict = {
+                'first_name': self.first_name,
+                'last_name': self.last_name,
+                'age': self.age
+            }
+
+        return newdict
