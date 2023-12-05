5b7977a0e6601e51c8830b37be47f7e6e32b9d66
n-elmasry <almassrynada411@gmail.com>
2023-12-05 10:27:52 -0800

    tasks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """script to add all arguments to a Python list and save to a file"""
 from sys import argv
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
