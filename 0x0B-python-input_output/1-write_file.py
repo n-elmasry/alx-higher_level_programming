f2d9a079663746b1604a28fbbdc2e62a13b80a05
n-elmasry <almassrynada411@gmail.com>
2023-12-05 07:01:25 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+"""  writes a string to a text file and returns the number of characters written """
 
+
+def write_file(filename="", text=""):
+    """ write to a text file """
+    with open(filename, 'w', encoding='utf-8') as fle:
+        fle.write(text)
+        num = len(text) + 1
+ 
+    return num
