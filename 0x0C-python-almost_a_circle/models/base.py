50d26b93760bb7ecb7c7e4191c1344877c564465
n-elmasry <almassrynada411@gmail.com>
2023-12-11 10:05:30 -0800

    tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,6 +9,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ init funf """
         if id is not None:
             self.id = id
         else:
@@ -18,6 +19,7 @@ class Base:
     # json static method
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ json static method """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
@@ -27,22 +29,19 @@ class Base:
     # class methos save_to_file
     @classmethod
     def save_to_file(cls, list_objs):
-        if (
-            list_objs is None
-            or not isinstance(list_objs, list)
-            or len(list_objs) == 0
-        ):
-            jsstring = "[]"
-        else:
-            jsstring = cls.to_json_string(
-                [obj.to_dictionary() for obj in list_objs]
-            )
-            with open(f'{cls.__name__}.json', 'w') as f:
-                f.write(jsstring)
+        """ class methos save_to_file """
+        filename = cls.__name__ + ".json"
+        string = []
+        if list_objs is not None:
+            for i in list_objs:
+                string.append(cls.to_dictionary(i))
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(string))
 
     # static method from_json_string
     @staticmethod
     def from_json_string(json_string):
+        """ static method from_json_string """
         if json_string is None or len(json_string) == 0:
             return []
         else:
@@ -52,6 +51,7 @@ class Base:
     # class method create
     @classmethod
     def create(cls, **dictionary):
+        """ class method create """
         dummy = cls(**dictionary)
         dummy.update(**dictionary)
         return dummy
@@ -59,6 +59,7 @@ class Base:
     # calss method load_from_file
     @classmethod
     def load_from_file(cls):
+        """ calss method load_from_file """
         filename = f'{cls.__name__}.json'
         if not os.path.exists(filename):
             return []
