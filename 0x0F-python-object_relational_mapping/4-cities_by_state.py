dc8a7bbc8acc6bf8cc211db9f39b5f375063a1c6
n-elmasry <almassrynada411@gmail.com>
2024-02-17 02:42:02 +0200

    task4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" lists all cities from the database hbtn_0e_4_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM cities ORDER BY cities.id ASC""")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
