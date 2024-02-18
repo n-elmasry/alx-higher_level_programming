f7d5e886b6851599b607da752040ca30857c00ae
n-elmasry <almassrynada411@gmail.com>
2024-02-18 02:23:15 +0200

    task12

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""prints the State object with the name passed as argument"""
+"""adds the State object “Louisiana” to the database """
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
