743f3dda7966fc5b593e76f546a09b908ba649ad
n-elmasry <almassrynada411@gmail.com>
2024-03-01 22:22:29 +0200

    task 0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """displays the value of the X-Request-Id variable"""
-from urllib import request
-from sys import argv
 
-url = argv[1]
+if __name__ == "__main__":
+    from urllib import request
+    from sys import argv
 
-with request.urlopen(url) as response:
-    header = response.headers
-
-    var = header.get('X-Request-Id')
-    if var is not None:
-        print(var)
+    url = argv[1]
+    with request.urlopen(url) as response:
+        header = response.headers
+        var = header.get('X-Request-Id')
+        if var is not None:
+            print(var)
