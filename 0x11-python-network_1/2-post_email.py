4152ab442e18af94aec3ce713f7322bac2acb4a1
n-elmasry <almassrynada411@gmail.com>
2024-03-01 22:58:28 +0200

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL"""
+
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    # encode the email
+    data = parse.urlencode({'email': email}).encode
+
+    req = request.Request(url, data=data, method='POST')
+
+    with request.urlopen(req) as response:
+        data = response.read().decode('utf-8')
+
+    print(data)
