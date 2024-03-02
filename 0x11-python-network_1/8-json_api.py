44dc3143bc8136160d64f121b62e3312a3631e34
n-elmasry <almassrynada411@gmail.com>
2024-03-02 02:15:45 +0200

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""uses the GitHub API to display your id"""
-
-if __name__ == "__main__":
+"""sends a POST request to http://0.0.0.0:5000/search_user"""
+if __name__ == '__main__':
     import requests
     from sys import argv
 
-    username = argv[1]
-    password = argv[2]
-    url = 'https://api.github.com/user'
+    url = 'http://0.0.0.0:5000/search_user'
+    q = argv[1] if len(argv) > 1 else ''
+
+    req = requests.post(url, data={'q': q})
+
     try:
-        response = requests.get(url, auth=(username, password))
-        data = response.json()
-        print(data['id'])
-    except Exception as err:
-        print('None')
+        data = req.json()
+        if not data:
+            print('No result')
+        else:
+            print(f"[{data.get('id')}] {data.get('name')}")
+
+    except ValueError:
+        print('Not a valid JSON')
