684668f3cf579fab0649ab8adeebec3c464a456f
n-elmasry <almassrynada411@gmail.com>
2024-03-02 02:30:12 +0200

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 """list 10 commits (from the most recent to oldest) """
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    req = requests.get(
+        "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2]))
+    commits = req.json()
+
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
