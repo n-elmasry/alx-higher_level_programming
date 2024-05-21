d997da69d9be734d5ac4c031ea763b9d4a3f8424
n-elmasry <almassrynada411@gmail.com>
2024-05-21 23:52:49 +0500

    task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,6 @@ const url = process.argv[2];
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
   }
   const films = JSON.parse(body).results;
   let count = 0;
